70b37ed25933702d7ff3977feef8db50f90e7951
Gabriele Rocha <56683790+gabrielerocha22@users.noreply.github.com>
2022-10-09 16:54:39 -0300

    Create SavingAccountPlus.java

---
+++ src/entities/SavingAccountPlus.java
@@ -0,0 +1,11 @@
+// package entities;
+
+// // Classe criada apenas para comprovar o uso do final em classes e métodos.  
+// public class SavingAccountPlus extends SavingAccount{
+	
+// 	@Override 
+// 	public void withdraw(Double amount) {
+// 		balance -= amount + 2;
+// 	}
+
+// }
